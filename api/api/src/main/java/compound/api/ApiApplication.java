6eb7a823673ab1ecfbb4de4d5ff77924b913c923
Krasen.Ilkov <krasen.ilkov@motion-software.com>
2020-01-16 17:09:30 +0200

    initial setup

---
+++ api/api/src/main/java/compound/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package compound.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
